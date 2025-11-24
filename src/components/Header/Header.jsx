c77809e7bd20b28d448b4508e57120a0286ef6dd
ResidentEvilGod <resmaster19@gmail.com>
2025-11-23 21:56:16 -0600

    Project 10 video 4

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import logo from "../../assets/logo.svg";
 import avatar from "../../assets/avatar.svg";
 import "./Header.css";
+import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
 function Header({ handleOpenAddGarmentModal, weatherData }) {
   const now = new Date();
   const dateStr = now.toLocaleDateString("default", {
@@ -10,24 +11,28 @@ function Header({ handleOpenAddGarmentModal, weatherData }) {
 
   return (
     <header className="header">
-      <img src={logo} alt="WTWR logo" className="header__logo" />
-      <time className="header__dateTime" dateTime={now}>
-        {dateStr}
-      </time>
-      <p className="header__place">, {weatherData.city}</p>
-
-      <button
-        onClick={handleOpenAddGarmentModal}
-        className="header__add-clothes-btn"
-      >
-        + Add clothes
-      </button>
-      <p className="header__username">Terrence Tegegne</p>
-      <img
-        src={avatar}
-        alt="Terrence Tegegne's Avatar"
-        className="header__avatar"
-      />
+      <div className="header__side">
+        <img src={logo} alt="WTWR logo" className="header__logo" />
+        <time className="header__dateTime" dateTime={now}>
+          {dateStr}
+        </time>
+        <p className="header__place">, {weatherData.city}</p>
+      </div>
+      <div className="header__side">
+        <ToggleSwitch />
+        <button
+          onClick={handleOpenAddGarmentModal}
+          className="header__add-clothes-btn"
+        >
+          + Add clothes
+        </button>
+        <p className="header__username">Terrence Tegegne</p>
+        <img
+          src={avatar}
+          alt="Terrence Tegegne's Avatar"
+          className="header__avatar"
+        />
+      </div>
     </header>
   );
 }
