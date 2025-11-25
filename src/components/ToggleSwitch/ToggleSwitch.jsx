a24240a706dca461f3d731184ed922a3fc0894b9
ResidentEvilGod <resmaster19@gmail.com>
2025-11-24 22:29:35 -0600

    Project 10 video 5

--- src/components/ToggleSwitch/ToggleSwitch.jsx
+++ src/components/ToggleSwitch/ToggleSwitch.jsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import "./ToggleSwitch.css";
 
 function ToggleSwitch() {
+  const [tempUnit, setTempUnit] = useState("F");
+
+  function handleChange() {
+    if (tempUnit == "F") {
+      setTempUnit("C");
+    } else {
+      setTempUnit("F");
+    }
+  }
+
   return (
     <label htmlFor="toggle-switch" className="toggle-switch">
       <input
         id="toggle-switch"
         type="checkbox"
         className="toggle-switch__checkbox"
+        onChange={handleChange}
       />
       <span className="toggle-switch__circle"></span>
       <span className="toggle-switch__value toggle-switch__value_left">F</span>
