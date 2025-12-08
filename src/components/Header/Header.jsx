15ffa58d3378af2ae87855e1947b098cb4174fa0
ResidentEvilGod <resmaster19@gmail.com>
2025-12-07 23:24:11 -0600

    Project 11 video 3

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,8 @@ import logo from "../../assets/logo.svg";
 import avatar from "../../assets/avatar.svg";
 import "./Header.css";
 import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
+import { Link } from "react-router-dom";
+
 function Header({ handleOpenAddGarmentModal, weatherData }) {
   const now = new Date();
   const dateStr = now.toLocaleDateString("default", {
@@ -12,7 +14,9 @@ function Header({ handleOpenAddGarmentModal, weatherData }) {
   return (
     <header className="header">
       <div className="header__side">
-        <img src={logo} alt="WTWR logo" className="header__logo" />
+        <Link to="/">
+          <img src={logo} alt="WTWR logo" className="header__logo" />
+        </Link>
         <time className="header__dateTime" dateTime={now}>
           {dateStr}
         </time>
@@ -26,12 +30,14 @@ function Header({ handleOpenAddGarmentModal, weatherData }) {
         >
           + Add clothes
         </button>
-        <p className="header__username">Terrence Tegegne</p>
-        <img
-          src={avatar}
-          alt="Terrence Tegegne's Avatar"
-          className="header__avatar"
-        />
+        <Link className="header__link" to="/profile">
+          <p className="header__username">Terrence Tegegne</p>
+          <img
+            src={avatar}
+            alt="Terrence Tegegne's Avatar"
+            className="header__avatar"
+          />
+        </Link>
       </div>
     </header>
   );
